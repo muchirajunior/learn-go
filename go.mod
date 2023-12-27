a14254c22379b37e1f1f807b76d527ae92df37dd
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2023-12-27 23:46:05 +0300

    variables

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module example/hello
+module learn-go
 
 go 1.21.5
--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	var nameOne = "junior"
+	var number int = 10
+	var decimal float32 = 283
+	var bigDecimal float64 = 66.09373
+	fmt.Println(nameOne, number, decimal, bigDecimal)
 }

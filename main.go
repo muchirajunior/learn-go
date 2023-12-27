618fe88f12cf78d5969239b7e6c764f3785c141b
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2023-12-28 00:39:43 +0300

    arrays

--- main.go
+++ main.go
@@ -3,9 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var nameOne = "junior"
-	var number int = 10
+	var nameOne = "muchira"
+	var number int8 = 10
 	var decimal float32 = 283
 	var bigDecimal float64 = 66.09373
-	fmt.Println(nameOne, number, decimal, bigDecimal)
+	nameTwo := "junior"
+	fmt.Println(nameOne, nameTwo, number, decimal, bigDecimal)
+
+	fmt.Println("my full name is", nameOne, nameTwo)
+
+	//formated string
+	fmt.Printf("FullName : %v - %v \n", nameOne, nameTwo)
+	fmt.Printf("FullName : %q - %q\n", nameOne, nameTwo)
+	fmt.Printf("number is type %T\n", number)
+	fmt.Printf("floting number %0.2f \n", bigDecimal)
+
+	//save formated string
+	formatedString := fmt.Sprintf("number is %v", number)
+	fmt.Println(formatedString)
+
+	//arrays
+	var numbers [4]int = [4]int{1, 2, 3}
+	numbers[3] = 100
+	fmt.Println(numbers, len(numbers))
+
+	//slices
+	var scores = []int{20, 40, 80}
+	scores[1] = 90
+	scores = append(scores, 88)
+	fmt.Println(scores, len(scores))
 }
